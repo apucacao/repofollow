57cf8152e4a232baeb813fbae9065ef3e7c32356
Alexis Georges <alexis.georges@gmail.com>
2014-08-10 14:52:18 -0700

    add keys to repo components

--- app/assets/javascripts/components/SearchResults.jsx
+++ app/assets/javascripts/components/SearchResults.jsx
@@ -16,13 +16,15 @@ function(_, React, Icon, Repository, StreamReactor) {
     mixins: [StreamReactor],
 
     render: function() {
+      var repos = _.map((repo) => Repository(_.mixin({key: repo.id}, repo)));
+
       return _.isAtom(this.state) ? (
         <section className="search-results">
           <header>
             <h3>Found {this.state.total_count} repositorie{this.state.total_count !== 1 ? 's' : ''} on GitHub</h3>
           </header>
           <div className="repos">
-            {_.map(Repository, this.state.items)}
+            {repos(this.state.items)}
           </div>
         </section>
       ) : null;
