b413d228cf3ca54e193aeffeb27e34719e973166
Alexis Georges <alexis.georges@gmail.com>
2014-08-12 20:19:06 -0700

    rework branch ui

--- app/assets/javascripts/components/Branch.jsx
+++ app/assets/javascripts/components/Branch.jsx
@@ -13,13 +13,11 @@ function(React, Icon, Button) {
   return React.createClass({
   	render: function() {
   		return (
-  			<li className="repo-branch row">
-          <span className="repo-branch-name cell">
+  			<li className="repo-branch">
+          <input type="checkbox" />
+          <span className="repo-branch-name">
             <Icon type="git-branch" />
             <a href={`http://github.com/${this.props.repo.owner.login}/${this.props.repo.name}/commits/${this.props.sha}`} title={`View ${this.props.repo.owner.login}/${this.props.repo.name} at ${this.props.name} on Github`}>{this.props.name}</a>
-          </span>
-          <span className="repo-follow-status cell">
-            <button className="btn btn-tiny"><Icon type="eye" /> Follow branch</button>
           </span>
         </li>
   		);
