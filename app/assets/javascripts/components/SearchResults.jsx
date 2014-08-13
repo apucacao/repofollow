7425b2a3313c528ad16be33cca8ddd88be0474e4
Alexis Georges <alexis.georges@gmail.com>
2014-08-12 20:29:17 -0700

    update count attribute

--- app/assets/javascripts/components/SearchResults.jsx
+++ app/assets/javascripts/components/SearchResults.jsx
@@ -19,7 +19,7 @@ function(_, React, Icon, RepositoryList, StreamReactor) {
       return _.isAtom(this.state) ? (
         <section className="search-results">
           <header>
-            <h3>Found {this.state.total_count} repositor{this.state.total_count !== 1 ? 'ies' : 'y'} on GitHub</h3>
+            <h3>Found {this.state.count} repositor{this.state.count !== 1 ? 'ies' : 'y'} on GitHub</h3>
           </header>
           <RepositoryList items={this.state.items} store={this.props.store} />
         </section>
