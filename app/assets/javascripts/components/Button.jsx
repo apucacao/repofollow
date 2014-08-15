d488578270c95aab6d9f101e59c6edf8c76136a8
Alexis Georges <alexis.georges@gmail.com>
2014-08-14 19:49:35 -0700

    richer button component

--- app/assets/javascripts/components/Button.jsx
+++ app/assets/javascripts/components/Button.jsx
@@ -13,18 +13,22 @@ function(React, Icon) {
     propTypes: {
       onClick: React.PropTypes.func,
       positive: React.PropTypes.bool,
+      disabled: React.PropTypes.bool,
+      size: React.PropTypes.oneOf(['tiny', 'big']),
       icon: React.PropTypes.string
     },
 
     render: function() {
       var classes = React.addons.classSet({
         'btn': true,
-        'btn-positive': this.props.positive
+        'btn-positive': this.props.positive,
+        'btn-tiny': this.props.size === 'tiny',
+        'btn-big': this.props.size === 'big'
       });
 
       var icon = this.props.icon ? <Icon type={this.props.icon} /> : null;
 
-      return <button className={classes} onClick={this.props.onClick}>{icon} {this.props.children}</button>;
+      return <button className={classes} onClick={this.props.onClick} disabled={this.props.disabled}>{icon} {this.props.children}</button>;
     }
   })
 
